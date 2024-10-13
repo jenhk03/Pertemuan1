c4397efb3d59958c1c143da891ede81fa1554c09
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2024-10-13 19:05:01 +0700

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    println("Hello World!")
+    println("\nNPM    : 2125250018")
+    println("Name   : Jendraja Husin Kotan")
+    println("Major  : Informatics")
+    println(4+2)
+    println(4-2)
+    println(4*2)
+    println(4/2)
+    println(3/2)
+    println(4.0/2.0)
+    println(3.0/2.0)
+}
